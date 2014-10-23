ff40378262d340bf785ab93432b634cf45c68c66
nirenjan <nirenjan@gmail.com>
2014-10-23 10:46:15 -0700

    Add include guard to require
    This prevents one file from being sourced more than once.

--- lib/overlord.sh
+++ lib/overlord.sh
@@ -1,16 +1,27 @@
 # Overlord master include file
 # This file includes all additional library files
 
+OVERLORD_SOURCE=''
+
 # This function sources the individual files relative to $OVERLORD_DIR/lib/
 require()
 {
     local file=$OVERLORD_DIR/lib/${1}.sh
+    local fn=${1//\//-}
 
     if [[ ! -e $file ]]
     then
         echo "overlord: fatal: cannot find required library file $1" >&2
         exit 1
     fi
+
+    # Include guard
+    if [[ "$OVERLORD_SOURCE" == *"$fn"* ]]
+    then
+        echo "overlord: warning: already included library file $1" >&2
+        return
+    fi
+    OVERLORD_SOURCE="$OVERLORD_SOURCE $fn"
 
     source $file
 }
